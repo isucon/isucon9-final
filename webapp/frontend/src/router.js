45299011dd1a779da0f313b52cd1731bdf479bf5
chibiegg <chibiegg@chibiegg.net>
2019-08-02 17:08:22 +0900

    トップメニュー

--- webapp/frontend/src/router.js
+++ webapp/frontend/src/router.js
@@ -5,6 +5,7 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
--- webapp/frontend/src/views/Home.vue
+++ webapp/frontend/src/views/Home.vue
@@ -1,8 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <section class="home">
+
+      <article class="reserve">
+        <p><a>予約</a></p>
+      </article>
+
+      <article class="inquiry">
+        <p><a>予約の確認</a></p>
+      </article>
+
+    </section>
+
+    <section class="information">
+      <article class="notice">
+        <h2>ご案内
+          <span>：　必ずお読みください</span>
+        </h2>
+        <div class="wrap1">
+          <div class="wrap2">
+            <p>
+              ■ここに何かお知らせをかく<br>
+              ・ホゲホゲホゲホゲ<br>
+              ・ふがふがふがふが<br>
+              ※ピヨピヨ
+            </p>
+          </div>
+        </div>
+        <p class="more">
+          <span id="l-9"></span>
+        </p>
+      </article>
+    </section>
   </div>
+
 </template>
 
 <script>
