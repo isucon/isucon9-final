cac6edffc3c743f55b8be28d2d2ea300092182f3
chibiegg <chibiegg@chibiegg.net>
2019-08-02 18:32:15 +0900

    v-model

--- webapp/frontend/src/views/Reservation.vue
+++ webapp/frontend/src/views/Reservation.vue
@@ -1,12 +1,92 @@
 <template>
   <div>
     <section class="search">
-      <Date/>
-      <Station/>
-      <Person/>
+
+      <article class="date">
+        <select class="year" v-model="year">
+          <option value="2020">2020年</option>
+          <option value="2021">2021年</option>
+        </select>
+        <select class="month" v-model="month">
+          <option value="1">1月</option>
+          <option value="2">2月</option>
+          <option value="3">3月</option>
+          <option value="4">4月</option>
+          <option value="5">5月</option>
+          <option value="6">6月</option>
+          <option value="7">7月</option>
+          <option value="8">8月</option>
+          <option value="9">9月</option>
+          <option value="10">10月</option>
+          <option value="11">11月</option>
+          <option value="12">12月</option>
+        </select>
+        <select class="day" v-model="day">
+          <option value="1">1日</option>
+          <option value="2">2日</option>
+          <option value="3">3日</option>
+          <option value="4">4日</option>
+          <option value="5">5日</option>
+          <option value="6">6日</option>
+          <option value="7">7日</option>
+          <option value="8">8日</option>
+          <option value="9">9日</option>
+          <option value="10">10日</option>
+          <option value="11">11日</option>
+          <option value="12">12日</option>
+          <option value="13">13日</option>
+          <option value="14">14日</option>
+          <option value="15">15日</option>
+          <option value="16">16日</option>
+          <option value="17">17日</option>
+          <option value="18">18日</option>
+          <option value="19">19日</option>
+          <option value="20">20日</option>
+          <option value="21">21日</option>
+          <option value="22">22日</option>
+          <option value="23">23日</option>
+          <option value="24">24日</option>
+          <option value="25">25日</option>
+          <option value="26">26日</option>
+          <option value="27">27日</option>
+          <option value="28">28日</option>
+          <option value="29">29日</option>
+          <option value="30">30日</option>
+          <option value="31">31日</option>
+        </select>
+      </article>
+
+      <article class="station">
+        <select class="from" v-model="from">
+          <option>新大阪</option>
+          <option>東京</option>
+        </select>
+        <span>→</span>
+        <select class="to" v-model="to">
+          <option>新大阪</option>
+          <option>東京</option>
+        </select>
+      </article>
+
+      <article class="person">
+        <select class="adult" v-model="adult">
+          <option value="0">おとな0名</option>
+          <option value="1">おとな1名</option>
+          <option value="2">おとな2名</option>
+          <option value="3">おとな3名</option>
+        </select>
+
+        <select class="child" v-model="child">
+          <option value="0">こども0名</option>
+          <option value="1">こども1名</option>
+          <option value="2">こども2名</option>
+          <option value="3">こども3名</option>
+        </select>
+      </article>
+
+
     </section>
     <section class="subcontent">
-
       <article class="notice">
         <h2>ご案内</h2>
         <p style="font-size: 11px;">
@@ -26,16 +106,19 @@
 </template>
 
 <script>
-import Date from '@/components/Reservation/Date.vue'
-import Station from '@/components/Reservation/Station.vue'
-import Person from '@/components/Reservation/Person.vue'
-
 export default {
   name: 'reservation',
-  components: {
-    Date,
-    Station,
-    Person
+  components: {},
+  data () {
+    return {
+      year: "2020",
+      month: "1",
+      day: "1",
+      from: "",
+      to: "",
+      adult: "1",
+      child: "0"
+    }
   }
 }
 </script>
@@ -54,6 +137,44 @@ export default {
 .search {
   float: left;
 }
+
+.search .date .year {
+    width: 214px;
+}
+
+.search .date .month {
+    width: 213px;
+}
+
+.search .date .day {
+    width: 213px;
+}
+
+.search .station .to,
+.search .station .from {
+    width: 300px;
+}
+
+.search .station span {
+  vertical-align: top;
+  padding: 0;
+  margin: 0;
+  height: 50px;
+  width: 40px;
+  display: inline-block;
+  text-align: center;
+  font-size: 24px;
+  background: #0057D3;
+  color: #ffffff;
+}
+
+.search .person .adult,
+.search .person .child {
+    width: 320px;
+}
+
+
+
 
 .subcontent {
   width: 317px;
