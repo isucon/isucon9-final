f0ebebc86d29e7cd5394072f4d486e85f7a3e210
h-kawabata <h-kawabata@sakura.ad.jp>
2019-08-03 04:10:32 +0000

    refactor

--- blackbox/payment/server/server.go
+++ blackbox/payment/server/server.go
@@ -1,100 +1,128 @@
 package server
 
 import (
-	_ "net/http/pprof"
 	"context"
+	_ "net/http/pprof"
 	"time"
 
 	"github.com/chibiegg/isucon9-final/blackbox/payment/config"
 	pb "github.com/chibiegg/isucon9-final/blackbox/payment/pb"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/rs/xid"
-	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-type Server struct{
-	PayInfoMap	map[string]*pb.PaymentInformation
+type Server struct {
+	PayInfoMap  map[string]*pb.PaymentInformation
 	CardInfoMap map[string]*pb.CardInformation
 }
 
 func NewNetworkServer(c *config.Config) (*Server, error) {
-	m := make(map[string]*pb.PaymentInformation,1000000)
-	c := make(map[string]*pb.CardInformation,1000000)
 	ns := &Server{
-		PayInfoMap: m,
-		CardInfomap: c,
+		PayInfoMap:  make(map[string]*pb.PaymentInformation, 1000000),
+		CardInfoMap: make(map[string]*pb.CardInformation, 1000000),
 	}
 	return ns, nil
 }
 
 //クレジットカードのトークン発行(非保持化対応)
 func (s *Server) RegistCard(ctx context.Context, req *pb.RegistCardRequest) (*pb.RegistCardResponse, error) {
-	guid := xid.New()
-	
-	cinfo := &pb.CardInformation{
-		CardNumber: req.CardInformation.CardNumber,
-		Cvv: req.CardInformation.Cvv,
-		ExpiryDate: req.CardInformation.ExpiryDate,
+	done := make(chan *pb.RegistCardResponse, 1)
+	ec := make(chan error, 1)
+	go func() {
+		if req.CardInformation == nil {
+			ec <- status.Errorf(codes.InvalidArgument, "Invalid POST data")
+			return
+		}
+		guid := xid.New()
+
+		s.CardInfoMap[guid.String()] = &pb.CardInformation{
+			CardNumber: req.CardInformation.CardNumber,
+			Cvv:        req.CardInformation.Cvv,
+			ExpiryDate: req.CardInformation.ExpiryDate,
+		}
+
+		done <- &pb.RegistCardResponse{CardToken: guid.String(), IsOk: true}
+	}()
+	select {
+	case r := <-done:
+		return r, nil
+	case err := <-ec:
+		return &pb.RegistCardResponse{IsOk: false}, err
 	}
-
-	s.CardInfoMap[guid.String()] = cinfo
-
-	return &pb.RegistCardResponse{
-		Token: guid.String(),
-		IsOk: true,
-	},nil
 }
 
 //決済を行う
 func (s *Server) ExecutePayment(ctx context.Context, req *pb.ExecutePaymentRequest) (*pb.ExecutePaymentResponse, error) {
-	date, err := ptypes.TimestampProto(time.Now())
-	if err != nil {
-		return nil,nil
-	}
-	guid := xid.New()
-	
-	info := &pb.PaymentInformation{
-		CardNumber: req.PaymentInformation.CardNumber,
-		Datetime: date,
-		Cvv: req.PaymentInformation.Cvv,
-		Amount: req.PaymentInformation.Amount,
-		IsCanceled: false,
-	}
+	done := make(chan *pb.ExecutePaymentResponse, 1)
+	ec := make(chan error, 1)
+	go func() {
+		if req.PaymentInformation == nil {
+			ec <- status.Errorf(codes.InvalidArgument, "Invalid POST data")
+			return
+		}
+		if _, ok := s.CardInfoMap[req.PaymentInformation.CardToken]; ok {
+			date, err := ptypes.TimestampProto(time.Now())
+			if err != nil {
+				ec <- err
+				return
+			}
+			guid := xid.New()
 
-	s.PayInfoMap[guid.String()] = info
+			s.PayInfoMap[guid.String()] = &pb.PaymentInformation{
+				CardToken:  req.PaymentInformation.CardToken,
+				Datetime:   date,
+				Amount:     req.PaymentInformation.Amount,
+				IsCanceled: false,
+			}
 
-	return &pb.ExecutePaymentResponse{
-		PaymentId: guid.String(),
-		IsOk: true,
-	},nil
+			done <- &pb.ExecutePaymentResponse{PaymentId: guid.String(), IsOk: true}
+		}
+		ec <- status.Errorf(codes.NotFound, "Card_Token Not Found")
+	}()
+	select {
+	case r := <-done:
+		return r, nil
+	case err := <-ec:
+		return &pb.ExecutePaymentResponse{IsOk: false}, err
+	}
 }
 
 //決済をキャンセルする
 func (s *Server) CancelPayment(ctx context.Context, req *pb.CancelPaymentRequest) (*pb.CancelPaymentResponse, error) {
-	if val, ok := s.PayInfoMap[req.PaymentId]; ok {
-		val.IsCanceled = true
-		return &pb.CancelPaymentResponse{
-			IsOk: true,
-		},nil
+	done := make(chan struct{}, 1)
+	ec := make(chan error, 1)
+	go func(){
+		if val, ok := s.PayInfoMap[req.PaymentId]; ok {
+			val.IsCanceled = true
+			
+			done <- struct{}{}
+		}
+		ec <- status.Errorf(codes.NotFound, "PaymentID Not Found")
+	}()
+	select {
+	case <- done:
+		return &pb.CancelPaymentResponse{IsOk: true}, nil
+	case err := <- ec:
+		return &pb.CancelPaymentResponse{IsOk: false}, err 
 	}
-	
-	return &pb.CancelPaymentResponse{
-		IsOk: false,
-	},status.Errorf(codes.NotFound,"PaymenID Not Found")
 }
 
 //決済情報を取得する
 func (s *Server) GetPaymentInformation(ctx context.Context, req *pb.GetPaymentInformationRequest) (*pb.GetPaymentInformationResponse, error) {
-	if val, ok := s.PayInfoMap[req.PaymentId]; ok {
-		return &pb.GetPaymentInformationResponse{
-			PaymentInformation: val,
-			IsOk: true,
-		},nil
+	done := make(chan *pb.GetPaymentInformationResponse, 1)
+	ec := make(chan error, 1)
+	go func(){
+		if val, ok := s.PayInfoMap[req.PaymentId]; ok {
+			done <- &pb.GetPaymentInformationResponse{PaymentInformation: val, IsOk: true}
+		}
+		ec <- status.Errorf(codes.NotFound, "PaymentID Not Found")
+	}()
+	select {
+	case r := <-done:
+		return r, nil
+	case err := <- ec:
+		return &pb.GetPaymentInformationResponse{PaymentInformation: nil, IsOk: false}, err
 	}
-
-	return &pb.GetPaymentInformationResponse{
-		PaymentInformation: nil,
-		IsOk: false,
-	},status.Errorf(codes.NotFound,"PaymenID Not Found")
 }
