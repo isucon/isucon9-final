34a9007da3ff6257810a6332dd44ec0914de810f
misodengaku <misodengaku@gmail.com>
2019-08-03 11:42:57 +0900

    座席マスタの情報がとりあえずJSONで返る

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var db *sql.DB
+
+type TrainSeat struct {
+	Row int
+	Column string
+	Class string
+	IsSmokingSeat bool
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
@@ -37,6 +46,53 @@ func distance_fare_handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
+
+func train_search_handler(w http.ResponseWriter, r *http.Request) {
+	/*
+		列車検索
+			GET /train/search?use_at=<ISO8601形式の時刻> & from=東京 & to=大阪
+	*/
+}
+
+func train_seats_handler(w http.ResponseWriter, r *http.Request) {
+	/*
+		指定した列車の座席列挙
+		GET /train/seats?train_class=のぞみ && train_name=96号
+	*/
+	rows, err := db.Query("SELECT seat_column,seat_row,seat_class,is_smoking_seat FROM seat_master WHERE train_class=? AND car_number=?", r.URL.Query().Get("train_class"), r.URL.Query().Get("car_num"))
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var seat_row int
+	var seat_column, seat_class string
+	var is_smoking_seat bool
+	var seats []TrainSeat
+	for rows.Next() {
+		err := rows.Scan(&seat_column, &seat_row, &seat_class, &is_smoking_seat)
+		if err != nil {
+			panic(err)
+		}
+		s := TrainSeat{seat_row, seat_column, seat_class, is_smoking_seat}
+		seats = append(seats, s)
+		
+		// fmt.Fprintf(w, "%d,%d\n", distance, fare)
+	}
+	resp, err := json.Marshal(s)
+	if err != nil {
+		panic(err)
+}
+	w.Write(resp)
+
+
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	// MySQL関連のお膳立て
 	var err error
@@ -48,7 +104,8 @@ func main() {
 
 	// HTTP
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/distance_fare", distance_fare_handler)
+	http.HandleFunc("/train/search", train_search_handler)
+	http.HandleFunc("/train/seats", train_seats_handler)
 
 	http.ListenAndServe(":8000", nil)
 }
