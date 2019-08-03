4f50457a2815087552c5efb7fcbef12319c29673
misodengaku <misodengaku@gmail.com>
2019-08-03 16:31:46 +0900

    上下線ともに検索できるようになった

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -8,9 +8,11 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	// "sync"
 )
 
 var db *sql.DB
+// var mu sync.Mutex
 
 type CarInformation struct {
 	Date time.Time
@@ -18,6 +20,13 @@ type CarInformation struct {
 	TrainName string
 	CarNumber int
 	SeatList []TrainSeat
+}
+
+type Train struct {
+	Class string
+	Name string
+	Start string
+	Last string
 }
 
 type TrainSeat struct {
@@ -57,13 +66,159 @@ func distance_fare_handler(w http.ResponseWriter, r *http.Request) {
 }
 
 
+/*
+func fare_calc(date time.Time, depStation, destStation, trainClass, seatClass string)
+{
+	//
+		// 料金計算メモ
+		// 距離運賃(円) * 期間倍率(繁忙期なら2倍等) * 車両クラス倍率(急行・各停等) * 座席クラス倍率(プレミアム・指定席・自由席)
+	//
+
+	
+	rows, err := db.Query("SELECT * FROM fare_master")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var tc int
+	var sc int
+	var d time.Time
+	var m float
+	for rows.Next() {
+		err := rows.Scan(&tc, &sc, &d, &m)
+		if err != nil {
+			panic(err)
+		}
+
+		// if 
+
+		fmt.Fprintf(w, "1234\n")
+	}
+
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+}
+*/
 
 func train_search_handler(w http.ResponseWriter, r *http.Request) {
 	/*
 		列車検索
 			GET /train/search?use_at=<ISO8601形式の時刻> & from=東京 & to=大阪
+		
+		return
+			料金
+			空席情報
+			発駅と着駅の到着時刻
+
 	*/
+	date, err := time.Parse(time.RFC3339, r.URL.Query().Get("use_at"))
+	if err != nil {
+		panic(err)
+	}
+	train_class := r.URL.Query().Get("train_class")
+	from := r.URL.Query().Get("from")
+	to := r.URL.Query().Get("to")
+
+	rows, err := db.Query("SELECT departure_at,train_class,train_name,start_station,last_station FROM train_master WHERE date=?", 
+		date.Format("2006-01-02"))
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	// var departure_at time.Time
+	var departure_at, train_name, start_station, last_station string
+	train_list := []Train{}
+	for rows.Next() {
+		err := rows.Scan(&departure_at, &train_class, &train_name, &start_station, &last_station)
+		if err != nil {
+			panic(err)
+		}
+		
+		var from_station_at, to_station_at float64
+		db.QueryRow("SELECT distance FROM station_master WHERE name=?", from).Scan(&from_station_at)
+		db.QueryRow("SELECT distance FROM station_master WHERE name=?", to).Scan(&to_station_at)
+		
+		// fmt.Println(from_station_at)
+		// fmt.Println(to_station_at)
+
+		query := "SELECT name FROM station_master ORDER BY distance"
+		if from_station_at > to_station_at {
+			// 上りだったら駅リストを逆にする
+			query += " DESC"
+		}
+		stations, err := db.Query(query)
+		if err != nil {
+			panic(err)
+		}
+		seeked_from_station := false
+		is_contains_from_station := false
+		is_contains_to_station := false
+		i := 0
+		for stations.Next() {
+			var v string
+			stations.Scan(&v)
+			// fmt.Println(v)
+
+			if !seeked_from_station {
+				// 駅リストを発駅まで読み飛ばして頭出しをする
+				if v == start_station {
+					seeked_from_station = true
+				} else {
+					continue	
+				}
+			}
+
+			if v == from {
+				// 発駅を経路中に持つ編成の場合フラグを立てる
+				is_contains_from_station = true
+				fmt.Println(v)
+			}
+			if v == to {
+				if is_contains_from_station {
+					// 発駅と着駅を経路中に持つ編成の場合
+					fmt.Println(v)
+					fmt.Println("---------")
+					is_contains_to_station = true
+					break
+				} else {
+					// 出発駅より先に終点が見つかったとき
+					// 上り対応したら要らなくなる
+					fmt.Println("なんかおかしい")
+					fmt.Println("---------")
+					break
+				}
+			}
+			if v == last_station {
+				// 駅が見つからないまま当該編成の終点に着いてしまったとき
+				break
+			}
+			i++
+		}
+		stations.Close()
+		if is_contains_from_station && is_contains_to_station {
+			// TODO: 料金計算
+			// TODO: 空席情報
+			// TODO: 所要時間計算
+			train_list = append(train_list, Train{train_class, train_name, start_station, last_station})
+		}
+	}
+	resp, err := json.Marshal(train_list)
+	if err != nil {
+		panic(err)
+	}
+	w.Write(resp)
+
+
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
 }
+
 
 func train_seats_handler(w http.ResponseWriter, r *http.Request) {
 	/*
@@ -117,7 +272,7 @@ func train_seats_handler(w http.ResponseWriter, r *http.Request) {
 	resp, err := json.Marshal(c)
 	if err != nil {
 		panic(err)
-}
+	}
 	w.Write(resp)
 
 
@@ -138,6 +293,7 @@ func main() {
 
 	// HTTP
 	http.HandleFunc("/", handler)
+	
 	http.HandleFunc("/train/search", train_search_handler)
 	http.HandleFunc("/train/seats", train_seats_handler)
 
