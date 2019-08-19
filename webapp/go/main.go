2ef96bcddc7c1beb2b1108c085daf80568ae6ef2
misodengaku <misodengaku@gmail.com>
2019-08-19 16:17:14 +0900

    camelcase

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -4,11 +4,12 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 	// "sync"
 )
 
@@ -43,7 +44,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
 }
 
-func distance_fare_handler(w http.ResponseWriter, r *http.Request) {
+func distanceFareHandler(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT * FROM distance_fare_master")
 	if err != nil {
 		panic(err)
@@ -104,7 +105,7 @@ func fare_calc(date time.Time, depStation, destStation, trainClass, seatClass st
 }
 */
 
-func train_search_handler(w http.ResponseWriter, r *http.Request) {
+func trainSearchHandler(w http.ResponseWriter, r *http.Request) {
 	/*
 		列車検索
 			GET /train/search?use_at=<ISO8601形式の時刻> & from=東京 & to=大阪
@@ -123,7 +124,7 @@ func train_search_handler(w http.ResponseWriter, r *http.Request) {
 	}
 	date = date.In(jst)
 
-	train_class := r.URL.Query().Get("train_class")
+	trainClass := r.URL.Query().Get("train_class")
 	from := r.URL.Query().Get("from")
 	to := r.URL.Query().Get("to")
 
@@ -134,24 +135,24 @@ func train_search_handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer rows.Close()
 
-	// var departure_at time.Time
-	var departure_at, train_name, start_station, last_station string
-	train_list := []Train{}
+	// var departureAt time.Time
+	var departureAt, trainName, startStation, lastStation string
+	trainList := []TrainSearchResponse{}
 	for rows.Next() {
-		err := rows.Scan(&departure_at, &train_class, &train_name, &start_station, &last_station)
+		err := rows.Scan(&departureAt, &trainClass, &trainName, &startStation, &lastStation)
 		if err != nil {
 			panic(err)
 		}
 
-		var from_station_at, to_station_at float64
-		db.QueryRow("SELECT distance FROM station_master WHERE name=?", from).Scan(&from_station_at)
-		db.QueryRow("SELECT distance FROM station_master WHERE name=?", to).Scan(&to_station_at)
+		var fromStationAt, toStationAt float64
+		db.QueryRow("SELECT distance FROM station_master WHERE name=?", from).Scan(&fromStationAt)
+		db.QueryRow("SELECT distance FROM station_master WHERE name=?", to).Scan(&toStationAt)
 
 		// fmt.Println(from_station_at)
 		// fmt.Println(to_station_at)
 
 		query := "SELECT name FROM station_master ORDER BY distance"
-		if from_station_at > to_station_at {
+		if fromStationAt > toStationAt {
 			// 上りだったら駅リストを逆にする
 			query += " DESC"
 		}
@@ -159,20 +160,20 @@ func train_search_handler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 		}
-		seeked_from_station := false
-		is_contains_from_station := false
-		is_contains_to_station := false
+		isSeekedToFirstStation := false
+		isContainsOriginStation := false
+		isContainsDestStation := false
 		i := 0
 		for stations.Next() {
 			var v string
 			stations.Scan(&v)
 			// fmt.Println(v)
 
-			if !seeked_from_station {
+			if !isSeekedToFirstStation {
 				// 駅リストを列車の発駅まで読み飛ばして頭出しをする
 				// 列車の発駅以前は止まらないので無視して良い
-				if v == start_station {
-					seeked_from_station = true
+				if v == startStation {
+					isSeekedToFirstStation = true
 				} else {
 					continue
 				}
@@ -180,15 +181,15 @@ func train_search_handler(w http.ResponseWriter, r *http.Request) {
 
 			if v == from {
 				// 発駅を経路中に持つ編成の場合フラグを立てる
-				is_contains_from_station = true
+				isContainsOriginStation = true
 				fmt.Println(v)
 			}
 			if v == to {
-				if is_contains_from_station {
+				if isContainsOriginStation {
 					// 発駅と着駅を経路中に持つ編成の場合
 					fmt.Println(v)
 					fmt.Println("---------")
-					is_contains_to_station = true
+					isContainsDestStation = true
 					break
 				} else {
 					// 出発駅より先に終点が見つかったとき
@@ -198,21 +199,21 @@ func train_search_handler(w http.ResponseWriter, r *http.Request) {
 					break
 				}
 			}
-			if v == last_station {
+			if v == lastStation {
 				// 駅が見つからないまま当該編成の終点に着いてしまったとき
 				break
 			}
 			i++
 		}
 		stations.Close()
-		if is_contains_from_station && is_contains_to_station {
+		if isContainsOriginStation && isContainsDestStation {
 			// TODO: 料金計算
 			// TODO: 空席情報
 			// TODO: 所要時間計算
-			train_list = append(train_list, Train{train_class, train_name, start_station, last_station})
+			trainList = append(trainList, Train{trainClass, trainName, startStation, lastStation})
 		}
 	}
-	resp, err := json.Marshal(train_list)
+	resp, err := json.Marshal(trainList)
 	if err != nil {
 		panic(err)
 	}
@@ -224,7 +225,7 @@ func train_search_handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func train_seats_handler(w http.ResponseWriter, r *http.Request) {
+func trainSeatsHandler(w http.ResponseWriter, r *http.Request) {
 	/*
 		指定した列車の座席列挙
 		GET /train/seats?train_class=のぞみ && train_name=96号
@@ -237,38 +238,38 @@ func train_seats_handler(w http.ResponseWriter, r *http.Request) {
 	}
 	date = date.In(jst)
 
-	train_class := r.URL.Query().Get("train_class")
-	train_name := r.URL.Query().Get("train_name")
-	car_number, err := strconv.Atoi(r.URL.Query().Get("car_num"))
+	trainClass := r.URL.Query().Get("train_class")
+	trainName := r.URL.Query().Get("train_name")
+	carNumber, err := strconv.Atoi(r.URL.Query().Get("car_num"))
 	if err != nil {
 		panic(err)
 	}
 
 	rows, err := db.Query("SELECT seat_column,seat_row,seat_class,is_smoking_seat FROM seat_master WHERE train_class=? AND car_number=?",
-		train_class, car_number)
+		trainClass, carNumber)
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
 
-	var seat_row int
-	var seat_column, seat_class string
-	var is_smoking_seat bool
+	var seatRow int
+	var seatColumn, seatClass string
+	var isSmokingSeat bool
 	var seats []TrainSeat
 	for rows.Next() {
-		err := rows.Scan(&seat_column, &seat_row, &seat_class, &is_smoking_seat)
+		err := rows.Scan(&seatColumn, &seatRow, &seatClass, &isSmokingSeat)
 		if err != nil {
 			panic(err)
 		}
 		var result int
 		db.QueryRow("SELECT COUNT(*) FROM seat_reservations WHERE date=? AND train_class=? AND train_name=? AND car_number=? AND seat_row=? AND seat_column=?",
 			date,
-			train_class,
-			train_name,
-			car_number,
-			seat_row,
-			seat_column).Scan(&result)
-		s := TrainSeat{seat_row, seat_column, seat_class, is_smoking_seat, false}
+			trainClass,
+			trainName,
+			carNumber,
+			seatRow,
+			seatColumn).Scan(&result)
+		s := TrainSeat{seatRow, seatColumn, seatClass, isSmokingSeat, false}
 		if result == 1 {
 			s.IsOccupied = true
 		}
@@ -276,7 +277,7 @@ func train_seats_handler(w http.ResponseWriter, r *http.Request) {
 
 		// fmt.Fprintf(w, "%d,%d\n", distance, fare)
 	}
-	c := CarInformation{date, train_class, train_name, car_number, seats}
+	c := CarInformation{date, trainClass, trainName, carNumber, seats}
 	resp, err := json.Marshal(c)
 	if err != nil {
 		panic(err)
@@ -334,8 +335,8 @@ func main() {
 	defer db.Close()
 
 	// HTTP
-	http.HandleFunc("/api/train/search", train_search_handler)
-	http.HandleFunc("/api/train/seats", train_seats_handler)
+	http.HandleFunc("/api/train/search", trainSearchHandler)
+	http.HandleFunc("/api/train/seats", trainSeatsHandler)
 
 	http.ListenAndServe(":8000", nil)
 }
