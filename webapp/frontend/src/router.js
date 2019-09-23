0e39cef7aeca2dec0df4ff9827377e9f4d864c68
Youta EGUSA <y-egusa@sakura.ad.jp>
2019-09-23 19:05:53 +0900

    雑な登録と削除

--- webapp/frontend/src/router.js
+++ webapp/frontend/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Register from './views/Register.vue'
+import Login from './views/Login.vue'
 import Reservation from './views/Reservation.vue'
 import Trains from './views/Trains.vue'
 
@@ -13,6 +15,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     },
     {
       path: '/reservation',
