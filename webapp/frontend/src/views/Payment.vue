01f89d760d709137a7a1e4019739c5fc6fd8a8df
chibiegg <chibiegg@chibiegg.net>
2019-09-28 15:53:37 +0900

    予約詳細コンポーネントに切り出し

--- webapp/frontend/src/views/Payment.vue
+++ webapp/frontend/src/views/Payment.vue
@@ -10,63 +10,9 @@
     </article>
   </section>
 
-  <section class="subcontent">
-    <article class="condition">
-      <div class="date">{{ year }}年{{ month }}月{{ day }}日</div>
-      <div class="station">{{ reservation.departure }}→{{ reservation.arrival }}</div>
-      <div class="person">おとな {{ reservation.adult }} 名 こども {{ reservation.child }} 名</div>
-    </article>
-  </section>
-
-
-
-  <section class="trains">
-    <article class="train-item">
-      <div class="wrap">
-
-        <div class="train">
-          <div class="departure">
-            <span class="time">{{ departure_time.getHours() }}時{{ departure_time.getMinutes() }}分 発</span>
-            <span class="station">{{ reservation.departure }}</span>
-          </div>
-
-          <div class="name">
-            <span class="name">{{ reservation.train_class }} {{ reservation.train_name }} 号</span>
-            <span class="type">N700系/全席禁煙</span>
-          </div>
-
-          <div class="arrival">
-            <span class="time">{{ arrival_time.getHours() }}時{{ arrival_time.getMinutes() }}分 着</span>
-            <span class="station">{{ reservation.arrival }}</span>
-          </div>
-
-          <div class="seats">
-
-            <h3>{{ seat_class_name }}</h3>
-
-
-            <div v-if="reservation.seat_class != 'non-reserved'">
-              <p v-for="(seat, index) in reservation.seats" style="margin-top: 0; margin-bottom: 0;">
-                {{ reservation.car_number }}号車{{ seat.seat_row }}番{{ seat.seat_column }}席
-              </p>
-            </div>
-
-          </div>
-
-        </div>
-      </div>
-    </article>
-  </section>
-  <section class="price">
-    <article class="price-item">
-      <div class="wrap">
-        <p>おとな</p><p style="text-align: right; margin-top: -30px;">{{ reservation.adult }}名分</p>
-        <p>こども</p><p style="text-align: right; margin-top: -30px;">{{ reservation.child }}名分</p>
-        <p>合計</p><p style="text-align: right; margin-top: -30px;">¥{{ reservation.amount }}</p>
-      </div>
-    </article>
-  </section>
-
+  <ReservationDetail
+    v-bind:reservation="reservation"
+  />
 
   <section class="information">
     <article>
@@ -125,8 +71,10 @@
 <script>
 import Router from '@/router.js'
 import { apiService } from '../services/api.js'
+import ReservationDetail from '@/components/Reservation/ReservationDetail.vue'
 
 export default {
+  components: {ReservationDetail},
   data: function() {
     return {
       reservation_id: 0,
@@ -137,7 +85,6 @@ export default {
       expiry_date_year: "24"
     }
   },
-  components: {},
   computed: {
     year() { return this.reservation.date.getYear() + 1900},
     month() { return this.reservation.date.getMonth() + 1 },
