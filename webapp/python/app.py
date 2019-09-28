4f99ff16343e2f793263730f6c97418a085dd1c0
chibiegg <chibiegg@chibiegg.net>
2019-09-28 13:53:02 +0900

    initialize, settings

--- webapp/python/app.py
+++ webapp/python/app.py
@@ -1,9 +1,40 @@
 import flask
+import os
+import MySQLdb.cursors
 
 app = flask.Flask(__name__)
 
+def dbh():
+    if not hasattr(flask.g, 'db'):
+        flask.g.db = MySQLdb.connect(
+            host=os.getenv('MYSQL_HOSTNAME', 'localhost'),
+            port=int(os.getenv('MYSQL_PORT', 3306)),
+            user=os.getenv('MYSQL_USER', 'isutrain'),
+            password=os.getenv('MYSQL_PASSWORD', 'isutrain'),
+            db=os.getenv('MYSQL_DATABASE', 'isutrain'),
+            charset='utf8mb4',
+            cursorclass=MySQLdb.cursors.DictCursor,
+            autocommit=True,
+        )
+    return flask.g.db
+
+@app.route("/api/settings", methods=["GET"])
+def get_settings():
+    return flask.jsonify({
+        "payment_api": os.getenv('PAYMENT_API', 'http://localhost:5000'),
+    })
+
+
+
 @app.route("/initialize", methods=["POST"])
 def post_initialize():
+
+    conn = dbh()
+    with conn.cursor() as c:
+        c.execute("TRUNCATE seat_reservations")
+        c.execute("TRUNCATE reservations")
+        c.execute("TRUNCATE users")
+
     return flask.jsonify({
         "language": "python" # 実装言語を返す
     })
