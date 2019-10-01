6e29e0bc15c42c4f1d2aff6cc27468a87a72bf13
chibiegg <chibiegg@chibiegg.net>
2019-10-01 11:31:16 +0900

    Python get_stations()

--- webapp/python/app.py
+++ webapp/python/app.py
@@ -1,4 +1,5 @@
 import flask
+import requests
 import os
 import MySQLdb.cursors
 
@@ -7,6 +8,21 @@ app = flask.Flask(__name__)
 
 AvailableDays = 10
 SessionName   = "session_isutrain"
+
+
+class HttpException(Exception):
+    status_code = 500
+
+    def __init__(self, status_code, message):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
+
+    def get_response(self):
+        response = flask.jsonify({'is_error': True, 'message': self.message})
+        response.status_code = self.status_code
+        return response
+
 
 
 def dbh():
@@ -23,10 +39,47 @@ def dbh():
         )
     return flask.g.db
 
+def filter_dict_keys(d, allowed_keys):
+    ret = {}
+    for k, v in d.items():
+        if k in allowed_keys:
+            ret[k] = v
+    return ret
+
+
+def http_json_error(code, msg):
+    raise HttpException(code, msg)
+
+@app.errorhandler(HttpException)
+def handle_http_exception(error):
+    return error.get_response()
+
 
 @app.route("/api/stations", methods=["GET"])
 def get_stations():
-    pass
+
+    station_list = []
+
+    try:
+        conn = dbh()
+        with conn.cursor() as c:
+            sql = "SELECT * FROM `station_master` ORDER BY id"
+            c.execute(sql)
+
+            while True:
+                station = c.fetchone()
+
+                if station is None:
+                    break
+
+                station = filter_dict_keys(station, ["id", "name", "is_stop_express", "is_stop_semi_express", "is_stop_local"])
+                station_list.append(station)
+
+    except MySQLdb.Error as err:
+        app.logger.exception(err)
+        http_json_error(requests.codes['internal_server_error'], "db error")
+
+    return flask.jsonify(station_list)
 
 
 @app.route("/api/train/search", methods=["GET"])
